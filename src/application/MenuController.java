5602e5d825f9178e874a7bc28c90bef678574a2c
Marc Merino <marcmerino24@gmail.com>
2021-04-20 16:47:11 +0200

    Added stuff...

--- src/application/MenuController.java
+++ src/application/MenuController.java
@@ -103,6 +103,23 @@ public class MenuController implements Initializable {
 		}
 	}
 	
+	@FXML
+    void handleCreateCycleScreen(ActionEvent event) {
+		AnchorPane root;
+		
+		try {
+			root = (AnchorPane)FXMLLoader.load(getClass().getResource("createCyclesScreen.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Main.escenario.setScene(scene);
+			Main.escenario.setTitle("Create Cycles");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
